e653e991ce06e9d6a3d91313e3699878ff8f5434
Pete K <krautscheid@mitre.org>
2015-09-15 00:19:39 -0400

    Add bootstrap and setup top menu-bar navigation

--- src/components/ReactBmiApp.js
+++ src/components/ReactBmiApp.js
@@ -4,22 +4,35 @@ const React = require('react/addons');
 const Router = require('react-router');
 const { RouteHandler, Link } = Router;
 
+// We could use a more React-y form of bootstrap
+//const ReactBootstrap = require('react-bootstrap');
+//const { Nav } = ReactBootstrap;
+
 // CSS
 require('normalize.css');
 require('../styles/main.css');
+require('bootstrap/dist/css/bootstrap.min.css');
 
 var ReactBmiApp = React.createClass({
+
+  contextTypes: {
+    router: React.PropTypes.func
+  },
+
   render: function() {
+    let currentPath = this.context.router.getCurrentPath();
+    let homeClass = React.addons.classSet({ 'nav-item': true, 'active': currentPath === '/' });
+    let aboutClass = React.addons.classSet({ 'nav-item': true, 'active': currentPath === '/about' });
     return (
-      <div class="container">
-        <nav class="navbar navbar-light bg-faded">
-          <Link class="navbar-brand" to="/">OHS JS Bakeoff</Link>
-          <ul class="nav navbar-nav">
-            <li class="nav-item">
-              <Link class="nav-link" to="/">Home</Link>
+      <div className="container">
+        <nav className="navbar navbar-light bg-faded">
+          <Link className="navbar-brand" to="/">OHS JS Bakeoff</Link>
+          <ul className="nav nav-pills">
+            <li className={homeClass}>
+              <Link className="nav-link" to="/">Home</Link>
             </li>
-            <li class="nav-item active">
-              <Link class="nav-link" to="about">About<span class="sr-only">(current)</span></Link>
+            <li className={aboutClass}>
+              <Link className="nav-link" to="about">About</Link>
             </li>
           </ul>
         </nav>
@@ -27,6 +40,7 @@ var ReactBmiApp = React.createClass({
       </div>
     );
   }
+
 });
 
 module.exports = ReactBmiApp;
